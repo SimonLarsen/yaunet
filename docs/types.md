1b013362fe478a5e766f7fd8dfe1e68391e1df96
Simon J. Larsen <sjl@esoft.com>
2026-08-25 07:25:16 +0000

    Document type aliases

---
+++ docs/types.md
@@ -0,0 +1,7 @@
+::: yaunet.types.FuseMethod
+::: yaunet.types.UpsampleMethod
+::: yaunet.types.InterpolationMethod
+
+::: yaunet.types.BlockConstructor
+::: yaunet.types.ActConstructor
+::: yaunet.types.NormConstructor
--- yaunet/types.py
+++ yaunet/types.py
@@ -3,11 +3,44 @@ from typing import Literal, TypeAlias
 
 from torch import nn
 
+FuseMethod: TypeAlias = Literal["add", "concat"]
+"""
+Methods for fusing skip connections.
+
+Attributes
+----------
+add
+    Fuse by element-wise addition.
+concat
+    Fuse by concatenation followed by linear projection.
+"""
+
+UpsampleMethod: TypeAlias = Literal["interpolate", "pixel-shuffle"]
+"""
+Methods for upsampling features.
+
+Attributes
+----------
+interpolate
+    Interpolation.
+pixel-shuffle
+    Linear projection followed by pixel shuffle.
+"""
+
+InterpolationMethod: TypeAlias = Literal["bilinear", "nearest-exact"]
+"""
+Interpolation methods.
+
+Attributes
+----------
+bilinear
+    Bilinear interpolation.
+nearest-exact
+    Nearest neighbor interpolation.
+"""
+
 BlockConstructor: TypeAlias = Callable[[int, int | None], nn.Module]
 NormConstructor: TypeAlias = Callable[[int], nn.Module]
 ActConstructor: TypeAlias = Callable[[], nn.Module]
 MLPConstructor: TypeAlias = Callable[[int], nn.Module]
 
-FuseMethod: TypeAlias = Literal["add", "concat"]
-UpsampleMethod: TypeAlias = Literal["interpolate", "pixel-shuffle"]
-InterpolationMethod: TypeAlias = Literal["bilinear", "nearest-exact"]
